4917be1b45c95c63f03bf1c3a0997f1b4c82f17a
Melissa NAIDOO <mnaidoo@e4r8p10.wethinkcode.co.za>
2018-09-20 11:13:36 +0200

    libft

---
+++ ft_strcpy.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mnaidoo <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/06/20 12:05:48 by mnaidoo           #+#    #+#             */
+/*   Updated: 2017/06/20 12:05:52 by mnaidoo          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strcpy(char *dest, const char *src)
+{
+	int		count;
+
+	count = 0;
+	while (src[count] != '\0')
+	{
+		dest[count] = src[count];
+		count++;
+	}
+	dest[count] = 0;
+	return (dest);
+}
+
+t_list      list_size(t_list begin)
+{
+    int i = 0;
+    
+    while(begin)
+    {
+        begin = begin->next
+        i++;
+    }
+
+    return(i);
+}
